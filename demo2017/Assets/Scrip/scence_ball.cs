16077533c73c4909a13fb9c8a2550942b9df0aa7
tanapon <tanapon.mane@bumail.net>
2017-01-28 16:56:51 +0700

    Demo2017

---
+++ demo2017/Assets/Scrip/scence_ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scence_ball : MonoBehaviour {
+	public Text txtscore;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	int score = 0;
+	public void Dopushbutton(){
+		score++;
+		txtscore.text = "Score :" + score.ToString ();
+	}
+
+	public void DochangeScene(){
+		SceneManager.LoadScene (1);
+		
+	}
+}
